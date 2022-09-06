99da38fc24cc9c7484c3abf2be801dcd92e6b1ea
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-06 14:03:56 +0300

    Initial Commit

---
+++ ControlRoom/ControlRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlRoomApp.swift
+//  ControlRoom
+//
+//  Created by Amby on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ControlRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
