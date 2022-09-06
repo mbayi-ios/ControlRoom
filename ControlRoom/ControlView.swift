e77da59bf2752471d7167662c60774817c00f360
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-06 15:04:34 +0300

    first commit

---
+++ ControlRoom/ControlView.swift
@@ -0,0 +1,20 @@
+//
+//  ControlView.swift
+//  ControlRoom
+//
+//  Created by Amby on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlView()
+    }
+}
